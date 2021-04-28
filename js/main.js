d1ce4e1bb10614d016d8545030eea60ff909639e
Javier Echevarria <javierechevarria21@yahoo.com>
2021-04-28 09:10:11 -0400

    revise js header

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("topnav").style.top = "0";
+  } else {
+    document.getElementById("topnav").style.top = "-100px";
+  }
+  prevScrollpos = currentScrollPos;
+}
